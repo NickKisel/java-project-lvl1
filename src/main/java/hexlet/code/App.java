55363e478a25157977a632f51390e41767a26510
Nick <insherlolqq@gmail.com>
2021-11-16 20:53:57 +0300

    step 5

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli name = new Cli();
-        Cli.greetingsUser(name);
+        System.out.println("Please enter the game number and press Enter.\n" + "1 - Greet\n"
+                + "2 - Even\n" + "0 - Exit");
+        Scanner choiceGameScanner = new Scanner(System.in);
+        String choiceGameStr = choiceGameScanner.nextLine();
+        System.out.println("Your choice: " + choiceGameStr);
+        if ("1".equals(choiceGameStr)) {
+            Cli greetings = new Cli();
+            Cli.greetingsUser(greetings);
+        }
+        if ("2".equals(choiceGameStr)) {
+            Even gameTwo = new Even();
+            Even.parityCheck(gameTwo);
+        }
     }
 }
---
+++ src/main/java/hexlet/code/Even.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void parityCheck(Even even) {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner whatsYourName = new Scanner(System.in);
+        String nameUser = whatsYourName.nextLine();
+        System.out.println("Hello, " + nameUser + "!");
+
+        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+        int rightAnswer = 0;
+        final int answersForWin = 3;
+        final int lowerLine = 1;
+        final int upperLine = 30;
+        Scanner scanner = new Scanner(System.in);
+        while (rightAnswer < answersForWin) {
+            int randomNumber = (int) (lowerLine + Math.random() * upperLine);
+            System.out.println("Question: " + randomNumber);
+            String answer = scanner.nextLine();
+            System.out.println("Your answer: " + answer);
+            if (randomNumber % 2 == 0 && "yes".equals(answer) || randomNumber % 2 != 0 && "no".equals(answer)) {
+                System.out.println("Correct!");
+                rightAnswer++;
+            } else if (randomNumber % 2 != 0 && !"no".equals(answer)
+                    || randomNumber % 2 == 0 && !"yes".equals(answer)) {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n"
+                        + "Let's try again, " + nameUser + "!");
+            }
+        }
+        System.out.println("Congratulations, " + nameUser + "!");
+    }
+}
